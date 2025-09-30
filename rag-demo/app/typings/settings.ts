85eefa705897497420eede1de6a9d0bc72b32636
Caleb Mackey <caleb.mackey@bolton-menk.com>
2025-09-30 12:04:07 -0500

    chore(cleanup): removed old demo files

--- rag-demo/app/typings/settings.ts
+++
@@ -1,7 +0,0 @@
-export type ColorTheme = 'light' | 'dark'
-
-export type AppTheme = ColorTheme | 'auto'
-
-export interface UserSettings {
-  theme: AppTheme;
-}
--- rag-demo/app/typings/utility.ts
+++
@@ -1,8 +0,0 @@
-export type ExtractProperties<T, U> = {
-  [K in keyof T as T[K] extends U ? K : never]: (event: T[K]) => void
-}
-
-// @ts-ignore
-export type NonFunctionProperties<T> = {
-  [K in keyof T as T[K] extends Function ? never : K]: T[K]
-}
