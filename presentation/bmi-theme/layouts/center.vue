61c86040a205bdf2cb77263417699d9d245df9c8
Caleb Mackey <caleb.mackey@bolton-menk.com>
2025-09-30 22:48:01 -0500

    refactor: theme and slide cleanup

---
+++ presentation/bmi-theme/layouts/center.vue
@@ -0,0 +1,32 @@
+<script lang="ts" setup>
+import BrandLogo from "../components/BrandLogo.vue";
+</script>
+
+<template>
+  <div class="slidev-layout center center-bmi">
+    <div class="flex flex-col justify-center text-center bmi-layout--center">
+      <slot></slot>
+    </div>
+    <div class="bg-bmi-footer">
+      <BrandLogo />
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+.bmi-layout--center {
+   > * img {
+    margin: 0 auto;
+  }
+}
+.bg-bmi-footer {
+  z-index: 1;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  height: 48px;
+  width: 100%;
+  background-color: var(--bmi-green) !important;
+}
+</style>
